a1a469777e148b371ab2f00126dd87effe64254a
anstjgus922 <msh176125@naver.com>
2025-01-04 23:39:24 +0900

    feat : main 배경 생성

---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./../styles/mainPage.css";
+
+const MainPage: React.FC = () => {
+  return (
+    <div className="main-page">
+      <div className="gradient-rectangle"></div>
+    </div>
+  );
+};
+
+export default MainPage;
