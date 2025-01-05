792c7b7a350b6f9f6fc2711ec8430d774b4c06cf
anstjgus922 <msh176125@naver.com>
2025-01-05 17:12:09 +0900

    feat : 로고 및 서비스명 배치

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -5,6 +5,11 @@ const MainPage: React.FC = () => {
   return (
     <div className="main-page">
       <div className="gradient-rectangle"></div>
+
+      <div className="logo-container">
+        <img src="/LearnAIbleLogo.png" alt="LearnAIble Logo" className="logo" />
+        <h1 className="title">Learn<span className="highlight">AI</span>ble</h1>
+      </div>
     </div>
   );
 };
