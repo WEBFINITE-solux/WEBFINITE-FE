d06e05b3e9a2f699696c7297cff4359c0a3ae7a8
anstjgus922 <msh176125@naver.com>
2025-01-19 16:02:04 +0900

    feat : 로그아웃 모달창 제작
    - layout.tsx 로그아웃 버튼 클릭 시 로그아웃 모달 창 작동하도록 일부 코드 수정하였습니다. (주석 처리된 부분 및 UserContainer부분. 그 외 코드 및 디자인은 건들지 않았습니다!)

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "./component/layout";
+import LogoutModal from "./component/logoutModal";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            path: "layout",
+            element: <LogoutModal onClose={() => console.log("모달 닫기")} />,
+          },
+        ],
+      },
+]);
 
 export default router;
