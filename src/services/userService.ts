fe48e8117e87a04421a3ec39345c134052eb8d7c
anstjgus922 <msh176125@naver.com>
2025-01-25 23:31:30 +0900

    refactor : 코드 중복 제거

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -7,25 +7,32 @@ export const loginUser = async (requestData: { loginUserId: string; password: st
         },
         body: JSON.stringify(requestData),
       });
-  
+
       if (!response.ok) {
-        const contentType = response.headers.get("Content-Type");
-        if (contentType && contentType.includes("application/json")) {
-          const errorData = await response.json();
-          throw new Error(errorData.message || "로그인 요청 실패");
-        } else {
-          throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
-        }
+        await handleErrorResponse(response);
       }
-  
-      const contentType = response.headers.get("Content-Type");
-      if (contentType && contentType.includes("application/json")) {
-        return await response.json();
-      } else {
-        throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
-      }
+
+      return await parseJsonResponse(response);
     } catch (error) {
       console.error("로그인 요청 중 오류 발생:", error);
       throw error;
     }
-  };  
+  };
+
+  const parseJsonResponse = async (response: Response) => {
+    const contentType = response.headers.get("Content-Type");
+    if (contentType && contentType.includes("application/json")) {
+      return await response.json();
+    }
+    throw new Error("서버에서 예상치 못한 형식의 응답이 도착했습니다.");
+  };
+
+  const handleErrorResponse = async (response: Response) => {
+    const contentType = response.headers.get("Content-Type");
+    if (contentType && contentType.includes("application/json")) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "로그인 요청 실패");
+    } else {
+      throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
+    }
+  };
